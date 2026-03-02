98c298855d9ad7abc971c463f4f3f55a1bf0d765
Fird3 <fride.standal@outlook.com>
2026-03-02 13:59:29 +0100

    forelesning
    code-along til arbeidskrav 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import History from "../components/History"
 
 export default function Home(){
     const [search, setSearch]= useState()
+    const storedHistory = localStorage.getItem("search")
+    const [focused, setFocused]= useState(false)
+    // tom array så vi kan legge til ting når vi bruker search og local storage
+    const [history, setHistory]= useState(storedHistory ? JSON.parse(storedHistory) : [])
+
+    console.log("Denne kommer fra storage", storedHistory)
     
     const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
     // IKKE SKRIV DET PÅ DENNE MÅTEN
@@ -9,7 +16,11 @@ export default function Home(){
     // U DEAD
     
     // husk å legg til key
-    const apiKey = ''
+    const apiKey = import.meta.env.VITE_APP_API_KEY
+
+    useEffect(()=>{
+        localStorage.setItem("search", JSON.stringify(history))
+    }, [history])
     
 
     const getMovies = async()=>{
@@ -27,17 +38,31 @@ export default function Home(){
     const handleChange = (e) => {
         setSearch(e.target.value)
     }
+
+   
+    const handleSubmit = (e) =>{
+        // sann den ikke refresher
+        e.preventDefault()
+        // sann input fjerner tekst etter du har trykket på søk
+        e.target.reset()
+
+        setHistory((prev) => [...prev, search])
+
+    
+    }
+    console.log(history)
     return(
         <main>
             <h1>Forside</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Søk etter film:
-                    <input type="search" placeholder="Harry Potter" onChange={handleChange}></input>
+                    <input type="search" placeholder="Harry Potter" onChange={handleChange} onFocus={()=> setFocused(true)} /*onBlur={()=> setFocused(false)}*/></input>
                 </label>
+                {focused ? <History history={history} setSearch={setSearch}/> : null }
+                <button onClick={getMovies}>Søk</button>
             </form>
-            {/* Nå søker den baare når jeg trykker på knappen */}
-            <button onClick={getMovies}>Søk</button>
+            
         </main>
     )
 }
